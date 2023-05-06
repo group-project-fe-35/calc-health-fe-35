8b9f06ce320c397239c05c390aacf9902dd9e196
perdanaph <danangputro9@gmail.com>
2023-05-06 11:39:17 +0700

    fixing register and login

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,41 +1,61 @@
 import {putToLocalStorage, getFromLocalStorage} from './service.js'
 
-const namaDepan = document.getElementById('namadepan').value;
-const namaBelakang = document.getElementById('namabelakang').value;
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
-const konfirmasiPassword = document.getElementById('konfirmasi').value;
-const form = document.getElementById('form-register')
-const check = document.getElementById('check').checked;
 const btn_daftar = document.getElementById('daftar')
 
+const handleData = () => {
+    const namaDepan = document.getElementById('namadepan').value;
+    const namaBelakang = document.getElementById('namabelakang').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const konfirmasiPassword = document.getElementById('konfirmasi').value;
+    const check = document.getElementById('check').checked;
+    if (namaDepan === '' && namaBelakang === '' && email === '' && password === '' && konfirmasiPassword === '' && check == false) {
+        return {
+            namaDepan: null,
+            namaBelakang: null,
+            email: null,
+            password: null,
+            konfirmasiPassword: null,
+            check: false
+        }
+    }
+    return { namaBelakang, namaDepan, email, password, konfirmasiPassword, check }
+}
+
 const isPasswordMatch = (data) => {
-    return data === konfirmasiPassword ? true : false;
+    return data.password === data.konfirmasiPassword ? true : false;
 };
 
-const handleRegister = () => {
-    if (namaDepan != '' || namaBelakang != '' || email != '' || isPasswordMatch(password) || check == false) {
-        const user = {
-            namadepan: namaDepan,
-            namabelakang: namaBelakang,
-            email,
-            password,
-        };
-        
-        putToLocalStorage(user)
-        alert('You are Registered, Click OK to login')
+const validateFormData = (data) => {
+    if (data.namaDepan == null && data.namaBelakang == null && data.email == null && data.password == null && data.konfirmasiPassword == null && check == false) {
+        return false
+    }
+    return true
+}
+
+const handleRegister = (data) => {
+    if (!validateFormData(handleData())) {
+        alert ('isi semua field')
+    }
+    if (!isPasswordMatch(handleData())) {
+        return alert('password do not match')
+    }
+    const user = {
+        nama: data.namaDepan,
+        email: data.email,
+        password: data.password
+    }
+    console.log(user)
+    putToLocalStorage(user)
+    alert('You are Registered, Click OK to login')
         window.location.href = '../../login.html'
         form.reset
-    } else {
-        alert('Fill in the entire form, dont miss anything')
-        form.reset()
-    }
-
 }
 
 btn_daftar.addEventListener('click', (event) => {
     event.preventDefault()
-    handleRegister()
+    handleRegister(handleData())
+    console.log(handleData())
 })
 
 
